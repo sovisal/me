42af4d63ae684d3905683ff8e226bfd1aa543ad0
Sovisal <sovisalsokun@gmail.com>
2023-10-30 23:11:34 +0700

    add .htaccess

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: 'https://sovisal.github.io',
+    base: 'https://sovisal.github.io/me/',
     plugins: [
         react()
     ],
