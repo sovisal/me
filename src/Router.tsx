fa2271fc885a3a682dd354fad0488f331987229a
sovisal <sovisalsokun@gmail.com>
2023-10-30 22:02:44 +0700

    update .

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+// src/Router.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
